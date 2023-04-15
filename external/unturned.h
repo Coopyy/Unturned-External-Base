880b0bde6997254d29cb2d27b06c2dbacce2cb87
Coopyy <26663693+Coopyy@users.noreply.github.com>
2023-04-15 16:37:03 -0500

    no recoil

--- external/unturned.h
+++ external/unturned.h
@@ -10,6 +10,10 @@ namespace unturned
 		static mono_class_t* channel;
 		static mono_class_t* steamplayer;
 
+		static mono_class_t* equipment;
+		static mono_class_t* asset;
+		static mono_class_t* gun_asset;
+
 		inline void init()
 		{
 			provider = mono::find_class("Assembly-CSharp", "SDG.Unturned.Provider");
@@ -17,6 +21,10 @@ namespace unturned
 			player = mono::find_class("Assembly-CSharp", "SDG.Unturned.Player");
 			channel = mono::find_class("Assembly-CSharp", "SDG.Unturned.SteamChannel");
 			steamplayer = mono::find_class("Assembly-CSharp", "SDG.Unturned.SteamPlayer");
+
+			equipment = mono::find_class("Assembly-CSharp", "SDG.Unturned.PlayerEquipment");
+			asset = mono::find_class("Assembly-CSharp", "SDG.Unturned.ItemAsset");
+			gun_asset = mono::find_class("Assembly-CSharp", "SDG.Unturned.ItemGunAsset");
 		}
 	}
 
@@ -30,6 +38,15 @@ namespace unturned
 		static uintptr_t owner;
 		static uintptr_t admin;
 
+		static uintptr_t equipment;
+		static uintptr_t asset;
+
+		static uintptr_t rec_min_x;
+		static uintptr_t rec_max_x;
+		static uintptr_t rec_min_y;
+		static uintptr_t rec_max_y;
+
+
 		inline void init()
 		{
 			player = classes::player->find_field("_player")->offset();
@@ -38,6 +55,13 @@ namespace unturned
 			channel = classes::player->find_field("_channel")->offset();
 			owner = classes::channel->find_field("owner")->offset();
 			admin = classes::steamplayer->find_field("_isAdmin")->offset();
+			equipment = classes::player->find_field("_equipment")->offset();
+			asset = classes::equipment->find_field("_asset")->offset();
+
+			rec_min_x = classes::gun_asset->find_field("recoilMin_x")->offset();
+			rec_max_x = classes::gun_asset->find_field("recoilMax_x")->offset();
+			rec_min_y = classes::gun_asset->find_field("recoilMin_y")->offset();
+			rec_max_y = classes::gun_asset->find_field("recoilMax_y")->offset();
 		}
 	}
 
@@ -61,5 +85,13 @@ namespace unturned
 		auto owner = read<uintptr_t>(chnl + offsets::owner); // Player.player.channel.owner
 
 		return owner;
+	}
+
+	inline uintptr_t get_held_gun_asset()
+	{
+		auto plr = read<uintptr_t>(get_static_player() + offsets::player); // Player.player
+		auto eq = read<uintptr_t>(plr + offsets::equipment); // Player.player.equipment
+		auto asset = read<uintptr_t>(eq + offsets::asset); // Player.player.equipment.asset
+		return asset;
 	}
 }
--- externaltest.cpp
+++ externaltest.cpp
@@ -16,13 +16,34 @@ int main()
 
 	auto lp = unturned::get_local_steamplayer();
 
-	Log("Local SteamPlayer -> 0x%p", lp);
+	Log("Local SteamPlayer -> 0x%p\n", lp);
 
 	if (connected && !loading && lp)
 	{
-		write<int>(1, lp + unturned::offsets::admin);
-		Log("Set admin!");
-		Log("Use keys Shift + F1 & F7 for freecam & esp");
+		while (true)
+		{
+			write<int>(1, lp + unturned::offsets::admin);
+			
+			auto gun = unturned::get_held_gun_asset();
+			if (gun)
+			{
+				write<float>(0, gun + unturned::offsets::rec_min_x);
+				write<float>(0, gun + unturned::offsets::rec_max_x);
+				write<float>(0, gun + unturned::offsets::rec_min_y);
+				write<float>(0, gun + unturned::offsets::rec_max_y);
+			}
+
+			static bool hasLogged = false;
+			if (!hasLogged)
+			{
+				Log("Set admin!");
+				Log("Use keys Shift + F1 & F7 for freecam & esp");
+				Log("Guns that are equipped should now have no recoil");
+				hasLogged = true;
+			}
+
+			Sleep(1000);
+		}
 	}
 	else
 		Log("It seems like you're not in a server yet, run again when you are");
